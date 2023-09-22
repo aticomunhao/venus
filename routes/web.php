d89529b473e0c6b66eeda813bc483b849fe3e5c7
Moises <moisesshalon72@gmail.com>
2023-09-22 16:01:40 -0300

    Rota login

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Auth;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
-    return view('/auth/login');
-});
+Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
 
 
 Auth::routes();
