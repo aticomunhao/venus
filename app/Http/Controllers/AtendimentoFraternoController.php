cecba7ccc52b7ac04a285f8ec241af79dad5784e
Moises <moisesshalon72@gmail.com>
2023-10-23 18:05:00 -0300

    novos menus

--- app/Http/Controllers/AtendimentoFraternoController.php
+++ app/Http/Controllers/AtendimentoFraternoController.php
@@ -6,16 +6,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Carbon;
+
+use function Laravel\Prompts\select;
+
 class AtendimentoFraternoController extends Controller
 {
     public function index(Request $request){
 
-        //session()->put('usuario', [
-        //    'id_usuario'=> 7,
-        //    'sexo' => 1
-       // ]);
-        
-      // dd(session());
      
         $atendente = session()->get('usuario.id_pessoa');
 
@@ -23,57 +20,71 @@ class AtendimentoFraternoController extends Controller
 
         $nome = session()->get('usuario.nome');
 
+        $now = Carbon::now()->format('Y-m-d H:m:s');
 
-        //$grupo = DB::table('grupos AS g')->select('g.nome as nomeg', 'g.id_dia_semana', 'g.hr_inicio', 'g.hr_fim')
-                                  //  ->leftJoin('tipo_dia AS td', 'g.id_dia_semana', 'td.id')
-                               //     ->get();
+        $assistido = DB::table('atendimentos AS at')
+                    ->select('at.id AS ida', 'p1.id AS idas', 'p1.ddd', 'p1.celular', 'at.dh_chegada', 'at.dh_inicio', 'at.dh_fim', 'at.id_assistido', 'p1.nome_completo AS nm_1', 'at.id_representante', 'p2.nome_completo AS nm_2', 'at.id_atendente_pref', 'p3.nome_completo AS nm_3', 'at.id_atendente', 'p4.nome_completo AS nm_4', 'at.pref_tipo_atendente', 'ts.descricao', 'tx.tipo','pa.nome')
+                    ->leftJoin('atendentes AS att', 'at.id_atendente', 'att.id_pessoa')
+                    ->leftJoin('tipo_status_atendimento AS ts', 'at.status_atendimento', 'ts.id')
+                    ->leftJoin('pessoas AS p1', 'at.id_assistido', 'p1.id')
+                    ->leftJoin('pessoas AS p2', 'at.id_representante', 'p2.id')
+                    ->leftJoin('pessoas AS p3', 'at.id_atendente_pref', 'p3.id')
+                    ->leftJoin('pessoas AS p4', 'at.id_atendente', 'p4.id')
+                    ->leftJoin('tp_sexo AS tx', 'at.pref_tipo_atendente', 'tx.id')
+                    ->leftJoin('tp_parentesco AS pa', 'at.parentesco', 'pa.id')
+                    ->where('at.dh_chegada', '=', DB::raw('select min(at.dh_chegada) from atendimentos at where status_atendimento = 1'))
+                  //  ->where('tx.id', $pref_att)
+                   // ->orWhere ('at.pref_tipo_atendente', $atendente)
+                   // ->whereNull('at.pref_tipo_atendente')                                            
+                    ->groupby('at.id', 'p1.id', 'p2.nome_completo', 'p3.nome_completo', 'p4.nome_completo', 'ts.descricao', 'tx.tipo', 'pa.nome')
+                    ->get();        
 
-        //dd($nome);
-
-        $now = Carbon::now()->format('Y-m-d');
-
-
-        $assistido = DB::select("select 
-                        at.id  ida,
-                        p1.id  idas,
-                        p1.ddd,
-                        p1.celular, 
-                        at.dh_chegada,
-                        at.dh_inicio,
-                        at.dh_fim,
-                        at.id_assistido,
-                        p1.nome_completo nm_1,
-                        at.id_representante,
-                        p2.nome_completo nm_2,
-                        at.id_atendente_pref,
-                        p3.nome_completo nm_3,
-                        at.id_atendente, 
-                        p4.nome_completo nm_4, 
-                        at.pref_tipo_atendente, 
-                        ts.descricao, 
-                        tx.tipo,
-                        pa.nome
-                        from atendimentos at                    
-                        left join atendentes att on (at.id_atendente =  att.id_pessoa)
-                        left join tipo_status_atendimento ts on (at.status_atendimento = ts.id)
-                        left join pessoas p1 on (at.id_assistido = p1.id)
-                        left join pessoas p2 on (at.id_representante = p2.id)
-                        left join pessoas p3 on (at.id_atendente_pref = p3.id)
-                        left join pessoas p4 on (at.id_atendente = p4.id)
-                        left join tp_sexo tx on (at.pref_tipo_atendente = tx.id)
-                        left join tp_parentesco pa on (at.parentesco = pa.id)                                             
-                        where at.dh_chegada = (select MIN(at.dh_chegada) from atendimentos at where status_atendimento = 1)
-                        and tx.id = $pref_att
-                        or at.pref_tipo_atendente = $atendente                        
-                        
-                        group by  at.id, p1.id, p2.nome_completo, p3.nome_completo, p4.nome_completo, ts.descricao, tx.tipo, pa.nome                        
-                        ");
+                    //dd($now);
+      
 
     
                     
-//dd($assistido);
+
         return view ('/atendimento-assistido/atendendo', compact('assistido', 'atendente', 'now', 'nome'));
 
     
     }
 }
+
+//   $assistido = DB::select("select 
+//                         at.id  ida,
+//                         p1.id  idas,
+//                         p1.ddd,
+//                         p1.celular, 
+//                         at.dh_chegada,
+//                         at.dh_inicio,
+//                         at.dh_fim,
+//                         at.id_assistido,
+//                         p1.nome_completo nm_1,
+//                         at.id_representante,
+//                         p2.nome_completo nm_2,
+//                         at.id_atendente_pref,
+//                         p3.nome_completo nm_3,
+//                         at.id_atendente, 
+//                         p4.nome_completo nm_4, 
+//                         at.pref_tipo_atendente, 
+//                         ts.descricao, 
+//                         tx.tipo,
+//                         pa.nome
+//                         from atendimentos at                    
+//                         left join atendentes att on (at.id_atendente =  att.id_pessoa)
+//                         left join tipo_status_atendimento ts on (at.status_atendimento = ts.id)
+//                         left join pessoas p1 on (at.id_assistido = p1.id)
+//                         left join pessoas p2 on (at.id_representante = p2.id)
+//                         left join pessoas p3 on (at.id_atendente_pref = p3.id)
+//                         left join pessoas p4 on (at.id_atendente = p4.id)
+//                         left join tp_sexo tx on (at.pref_tipo_atendente = tx.id)
+//                         left join tp_parentesco pa on (at.parentesco = pa.id)                                             
+//                         where at.dh_chegada = (select MIN(at.dh_chegada) from atendimentos at where status_atendimento = 1)
+//                         and tx.id = $pref_att
+//                         or at.pref_tipo_atendente = $atendente
+//                         null at.pref_tipo_atendente
+
+                        
+//                         group by  at.id, p1.id, p2.nome_completo, p3.nome_completo, p4.nome_completo, ts.descricao, tx.tipo, pa.nome                        
+//                         ");
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,34 +1,43 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color:#87CEFA; font-family:tahoma; font-weight:bold;">
         <div class="container" >
-            <a class="navbar-brand" href="{{ url('/login/valida') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <div class="dropdown"  >
-                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Atendimento Espírita</button> 
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/gerenciar-atendimentos">Recepção Atendimento Fraterno</a></li>
-                        <li><a class="dropdown-item" href="#">Recepção Geral</a></li>
-                        <li><a class="dropdown-item" href="#">Gerenciar Atendentes</a></li>
-                        <li><a class="dropdown-item" href="/atendendo">Atender o assistido</a></li>
-                    </ul>
-                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;"> Administração</button> 
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/gerenciar-pessoas">Gerenciar pessoas</a></li>
-                        <li><a class="dropdown-item" href="#">Recepção Geral</a></li>
-                        <li><a class="dropdown-item" href="#">Gerenciar Atendentes</a></li>
-                        <li><a class="dropdown-item" href="/atendendo">Atender o assistido</a></li>
-                    </ul>
-            </div>
-            <div class="dropdown"  >
-                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Logout</button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/usuario/alterar-senha"><i class="mdi mdi-lock-open-outline font-size-17 text-muted align-middle mr-1"></i>Alterar Senha</a></li>
-                        <li><a class="dropdown-item text-danger" href="javascript:void();" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="mdi mdi-power font-size-17 text-muted align-middle mr-1 text-danger"></i> {{ __('Sair') }}</a></li>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </ul>
+            <div class="col-12">
+                <div class="btn-group">
+
+                    <a class="navbar-brand" href="{{ url('/login/valida') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+                <div class="btn-group">
+                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Atendimento Espírita</button> 
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/gerenciar-atendimentos">Recepção Atendimento Fraterno</a></li>
+                            <li><a class="dropdown-item" href="#">Recepção Geral</a></li>
+                            <li><a class="dropdown-item" href="#">Gerenciar Atendentes</a></li>
+                            <li><a class="dropdown-item" href="/atendendo">Atender o assistido</a></li>
+                            
+                        </ul>
+                </div>                    
+                <div class="btn-group">
+                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Administração</button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/gerenciar-pessoas">Gerenciar pessoas</a></li>
+                            <li><a class="dropdown-item" href="#">Gerenciar Atendentes</a></li>
+                            <li><a class="dropdown-item" href="/gerenciar-fatos">Gerenciar Fatos</a></li>
+                            <li><a class="dropdown-item" href="/gerenciar-grupos">Gerenciar Grupos</a></li>
+                            <li><a class="dropdown-item" href="/gerenciar-salas">Gerenciar Salas</a></li>
+                        </ul>                    
+                </div>
+                <div class="btn-group">
+                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#ffffff;">Logout</button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/usuario/alterar-senha"><i class="mdi mdi-lock-open-outline font-size-17 text-muted align-middle mr-1"></i>Alterar Senha</a></li>
+                            <li><a class="dropdown-item text-danger" href="javascript:void();" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="mdi mdi-power font-size-17 text-muted align-middle mr-1 text-danger"></i> {{ __('Sair') }}</a></li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </ul>
+                </div>
             </div> 
         </div>
     </nav>
