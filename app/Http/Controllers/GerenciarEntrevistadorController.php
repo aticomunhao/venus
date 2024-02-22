0d4eee5d297d3c248691fdb0c771136f8dd811e0
Daviddgabriel <gabrielgalvao10@gmail.com>
2024-02-22 17:02:14 -0300

    Entrevistador v1.2

---
+++ app/Http/Controllers/GerenciarEntrevistadorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Query\JoinClause;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Redirect;
+use PhpParser\Node\Expr\AssignOp\Coalesce;
+use PhpParser\Node\Expr\BinaryOp\Coalesce as BinaryOpCoalesce;
+
+class GerenciarEntrevistadorController extends Controller
+{
+
+    public function index(Request $request)
+    {
