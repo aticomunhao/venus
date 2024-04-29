197ce0731a7fb522e8fa4de692315265e84ebdff
nathanFAO <nathan1fellipe@gmail.com>
2024-04-29 17:13:25 -0300

    Avulsos e Presença

--- resources/views/recepcao-integrada/gerenciar-tratamentos.blade.php
+++ resources/views/recepcao-integrada/gerenciar-tratamentos.blade.php
@@ -1,122 +1,215 @@
 @extends('layouts.app')
 
-@section('title') Gerenciar Tratamentos @endsection
-
-@section('content')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.0.4/css/bootstrap5-toggle.min.css" rel="stylesheet">
-
-<?php
-//echo "<meta HTTP-EQUIV='refresh' CONTENT='30;URL=gerenciar-atendimentos'>";
-?>
-
-<div class="container";>
-<h4 class="card-title" class="card-title" style="font-size:20px; text-align: left; color: gray; font-family:calibri">GERENCIAR TRATAMENTOS</h4>
-    <div class="col-12">
-        <div class="row justify-content-center">
-            <div>
-                <form action="{{route('gtcdex')}}" class="form-horizontal mt-4" method="GET" >
-                <div class="row">
-                    <div class ="col-2">Data início
-                        <input class="form-control" type="date" id="" name="dt_enc" value="{{$data_enc}}">
-                    </div>
-                    <div class="col-1">Dia
-                        <select class="form-select" id="" name="dia" type="number">
-                            <option value=""></option>
-                            @foreach ($dia as $dias)
-                            <option value="{{$dias->id}}">{{$dias->nome}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col">Assistido
-                        <input class="form-control" type="text" id="3" name="assist" value="{{$assistido}}">
-                    </div>
-                    <div class="col-1">Status
-                        <select class="form-select" id="4" name="status" type="number">
-                            <option value="{{$situacao}}"></option>
-                            @foreach ($stat as $status)
-                            <option value="{{$status->id}}">{{$status->nome}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                        <div class="col"><br/>
-                            <input class="btn btn-light btn-sm me-md-2" style="box-shadow: 1px 2px 5px #000000; margin:5px;" type="submit" value="Pesquisar">
-                            <a href="/gerenciar-tratamentos"><input class="btn btn-light btn-sm me-md-2" style="box-shadow: 1px 2px 5px #000000; margin:5px;" type="button" value="Limpar"></a>
-                            <a href="/job"><input class="btn btn-light btn-sm me-md-2" style="box-shadow: 1px 2px 5px #000000; margin:5px;" type="button" value="Job"></a>
-                    </form>
-
-                        </div>
-                </div>
-                <br/>
-            </div style="text-align:right;">
-            <hr/>
-            <div class="table">Total assistidos: {{$contar}}
-                <table class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
-                    <thead style="text-align: center;">
-                        <tr style="background-color: #d6e3ff; font-size:14px; color:#000000">
-                            <th class="col">Nr</th>
-
-                            <th class="col">PRIORIDADE</th>
-                            <th class="col">ASSISTIDO</th>
-                            <th class="col">REPRESENTANTE</th>
-                            <th class="col">DIA</th>
-                            <th class="col">HORÁRIO</th>
-                            <th class="col">TRATAMENTO</th>
-                            <th class="col">GRUPO</th>
-                            <th class="col">STATUS</th>
-                            <th class="col">AÇÕES</th>
-                        </tr>
-                    </thead>
-                    <tbody style="font-size: 14px; color:#000000; text-align: center;">
-                        <tr>
-                        @foreach($lista as $listas)
-                            <td>{{$listas->idtr}}</td>
-                            <td>{{$listas->prdesc}}</td>
-                            <td>{{$listas->nm_1}}</td>
-                            <td>{{$listas->nm_2}}</td>
-                            <td>{{$listas->nomed}}</td>
-                            <td>{{date ('H:m:s', strtotime($listas->h_inicio))}}</td>
-                            <td>{{$listas->sigla}}</td>
-                            <td>{{$listas->nomeg}}</td>
-                            <td>{{$listas->tst}}</td>
-                            <td>
-                                <button class="btn btn-outline-warning btn-sm" type="button" id="" data-bs-toggle="modal" data-bs-target="#presenca{{$listas->idtr}}" data-tt="tooltip" data-placement="top" title="Presença"><i class="bi bi bi-exclamation-triangle" style="font-size: 1rem; color:#000;"></i></button>
-                                <a href="/visualizar-trat/{{$listas->idtr}}"><button type="button" class="btn btn-outline-primary btn-sm" data-tt="tooltip" data-placement="top" title="Histórico"><i class="bi bi-search" style="font-size: 1rem; color:#000;"></i></button></a>
-                                <a href="/alterar-grupo-tratamento/{{$listas->idtr}}"><button type="button" class="btn btn-outline-primary btn-sm"  data-tt="tooltip" data-placement="top" title="Alterar Grupo"><i class="bi bi-arrow-left-right" style="font-size: 1rem; color:#000;"></i></button></a>
-                                <a href="/inativar/{{$listas->idtr}}"><button type="button" class="btn btn-outline-danger btn-sm"  data-tt="tooltip" data-placement="top" title="Inativar"><i class="bi bi-x-circle" style="font-size: 1rem; color:#000;"></i></button></a>
-                            </td>
-                            @include('recepcao-integrada/pop-up-presenca')
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div class="d-flex justify-content-center">
-            {{$lista->withQueryString()->links()}}
-        </div>
-
-    </div>
-</div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-<script>
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-tt="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
-</script>
-
-
-
-
+@section('title')
+    Gerenciar Tratamentos
 @endsection
 
-@section('footerScript')
+@section('content')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.0.4/css/bootstrap5-toggle.min.css" rel="stylesheet">
+
+    <?php
+    //echo "<meta HTTP-EQUIV='refresh' CONTENT='30;URL=gerenciar-atendimentos'>";
+    ?>
+
+    <div class="container";>
+        <h4 class="card-title" class="card-title" style="font-size:20px; text-align: left; color: gray; font-family:calibri">
+            GERENCIAR TRATAMENTOS</h4>
+        <div class="col-12">
+            <div class="row justify-content-center">
+                <div>
+                    <form action="{{ route('gtcdex') }}" class="form-horizontal mt-4" method="GET">
+                        <div class="row">
+
+                            <div class ="col-2">Data início
+                                <input class="form-control" type="date" id="" name="dt_enc"
+                                    value="{{ $data_enc }}">
+                            </div>
+
+                            <div class="col-1">
+                                Dia
+                                <select class="form-select teste" id="" name="dia" type="number">
+                                    @foreach ($dia as $dias)
+                                        <option value="{{ $dias->id }}" {{ $diaP == $dias->id ? 'selected' : '' }}>
+                                            {{ $dias->nome }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="col">Assistido
+                                <input class="form-control" type="text" id="3" name="assist"
+                                    value="{{ $assistido }}">
+                            </div>
+
+                            <div class="col-1">Status
+                                <select class="form-select teste1" id="4" name="status" type="number">
+
+                                    @foreach ($stat as $status)
+                                        <option value="{{ $status->id }}"
+                                            {{ $situacao == $status->id ? 'selected' : '' }}> {{ $status->nome }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="col">
+                                <br />
+                                <input class="btn btn-light btn-sm me-md-2"
+                                    style="box-shadow: 1px 2px 5px #000000; margin:5px;" type="submit" value="Pesquisar">
+
+                                <a href="/gerenciar-tratamentos"><input class="btn btn-light btn-sm me-md-2"
+                                        style="box-shadow: 1px 2px 5px #000000; margin:5px;" type="button"
+                                        value="Limpar"></a>
+
+                                <a href="/job"><input class="btn btn-light btn-sm me-md-2"
+                                        style="box-shadow: 1px 2px 5px #000000; margin:5px;" type="button"
+                                        value="Job"></a>
+
+                                <a href="/incluir-avulso"><input class="btn btn-success btn-sm me-md-2" type="button"
+                                        value="Incluir Avulso"></a>
+
+                    </form>
+
+                </div>
+            </div>
+            <br />
+
+        </div style="text-align:right;">
+        <hr />
+        <div class="table">Total assistidos: {{ $contar }}
+            <table class="table table-sm table-striped table-bordered border-secondary table-hover align-middle">
+                <thead style="text-align: center;">
+                    <tr style="background-color: #d6e3ff; font-size:14px; color:#000000">
+                        <th class="col">Nr</th>
+                        <th class="col">PRIORIDADE</th>
+                        <th class="col">ASSISTIDO</th>
+                        <th class="col">REPRESENTANTE</th>
+                        <th class="col">DIA</th>
+                        <th class="col">HORÁRIO</th>
+                        <th class="col">TRATAMENTO</th>
+                        <th class="col">GRUPO</th>
+                        <th class="col">STATUS</th>
+                        <th class="col">AÇÕES</th>
+                    </tr>
+                </thead>
+                <tbody style="font-size: 14px; color:#000000; text-align: center;">
+                    <tr>
+                        @foreach ($lista as $listas)
+                            <td>{{ $listas->idtr }}</td>
+                            <td>{{ $listas->prdesc }}</td>
+                            <td>{{ $listas->nm_1 }}</td>
+                            <td>{{ $listas->nm_2 }}</td>
+                            <td>{{ $listas->nomed }}</td>
+                            <td>{{ date('H:i', strtotime($listas->h_inicio)) }}</td>
+                            <td>{{ $listas->sigla }}</td>
+                            <td>{{ $listas->nomeg }}</td>
+                            <td>{{ $listas->tst }}</td>
+                            <td>
+
+                                <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal"
+                                    data-tt="tooltip" data-placement="top" title="Presença"
+                                    data-bs-target="#presenca{{ $listas->idtr }}"><i class="bi bi bi-exclamation-triangle"
+                                        style="font-size: 1rem; color:#000;"></i></button>
+
+
+                                <div class="modal fade" id="presenca{{ $listas->idtr }}" tabindex="-1"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <form method="post" action="/presenca-tratatamento/{{ $listas->idtr }}">
+                                        @csrf
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header" style="background-color:orange;color:white">
+                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Registrar Presença
+                                                    </h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="mb-3">
+                                                        <label for="recipient-name" class="col-form-label"
+                                                            style="font-size:17px">Tem certeza que deseja registrar
+                                                            presença para<br /><span
+                                                                style="color:orange">{{ $listas->nm_1 }}</span>&#63;</label>
+                                                    </div>
+                                                    <center>
+                                                        <div class="mb-2 col-10">
+                                                            <label class="col-form-label">Insira o número de acompanhantes,
+                                                                <span style="color:orange">se necessário:</span></label>
+                                                            <input type="number" class="form-control"  name="acompanhantes"
+                                                                placeholder="0" min="0">
+                                                        </div>
+                                                    </center>
+
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-danger"
+                                                        data-bs-dismiss="modal">Cancelar</button>
+                                                    <button type="submit" class="btn btn-primary">Registrar
+                                                        Presença</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+
+
+
+
+
+
+
+                                <a href="/visualizar-trat/{{ $listas->idtr }}" type="button"
+                                    class="btn btn-outline-primary btn-sm" data-tt="tooltip" data-placement="top"
+                                    title="Histórico"><i class="bi bi-search"
+                                        style="font-size: 1rem; color:#000;"></i></a>
+
+                                <a href="/alterar-grupo-tratamento/{{ $listas->idtr }}"type="button"
+                                    class="btn btn-outline-primary btn-sm" data-tt="tooltip" data-placement="top"
+                                    title="Alterar Grupo"><i class="bi bi-arrow-left-right"
+                                        style="font-size: 1rem; color:#000;"></i></a>
+
+                                <a href="/inativar/{{ $listas->idtr }}" type="button"
+                                    class="btn btn-outline-danger btn-sm" data-tt="tooltip" data-placement="top"
+                                    title="Inativar"><i class="bi bi-x-circle"
+                                        style="font-size: 1rem; color:#000;"></i></a>
+
+                            </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div class="d-flex justify-content-center">
+        {{ $lista->withQueryString()->links() }}
+    </div>
+
+    </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            if ({{ $diaP == null }}) { //Deixa o select status como padrao vazio
+                $(".teste").prop("selectedIndex", -1);
+            }
+
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            if ({{ $situacao == null }}) { //Deixa o select status como padrao vazio
+                $(".teste1").prop("selectedIndex", -1);
+            }
+
+        });
+    </script>
+
+    <script>
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-tt="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+    </script>
 @endsection
---
+++ resources/views/recepcao-integrada/incluir-avulso.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('title')
+    Incluir Avulso
+@endsection
+
+@section('content')
+    <br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <form class="form-horizontal" method="post" action="/armazenar-avulso">
+                    @csrf
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col">
+                                INCLUIR AVULSO
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body ">
+
+                        <div class="mt-3 row">
+
+                            <div class=" mb-3 col-6">
+                                Nome Assistido
+                                <select class="form-select select2" aria-label="Default select example" name="assistido" required>
+
+                                    @foreach($assistidos as $assistido)
+                                    <option value="{{ $assistido->id }}">{{ $assistido->nome_completo }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class=" mb-3 col-6">
+                                Número de Acompanhantes
+                                <input type="number" class="form-control" name="acompanhantes"  placeholder="0">
+                            </div>
+                            <div class=" mb-3 col-12">
+                                Reunião Mediúnica
+                                <select class="form-select" aria-label="Default select example" name="reuniao" required>
+                                    @foreach($reuniao as $reunioes)
+                                    <option value="{{ $reunioes->id }}">{{ $reunioes->nome }} - {{ date('H:i', strtotime($reunioes->h_inicio)) }}/{{ date('H:i', strtotime($reunioes->h_fim)) }} - Sala {{ $reunioes->sala }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="row mb-3">
+                        <div class="d-grid gap-1 col-4 mx-auto">
+                            <a class="btn btn-danger" href="/gerenciar-tratamentos" role="button">Cancelar</a>
+                        </div>
+                        <div class="d-grid gap-2 col-4 mx-auto">
+                            <button type="submit" class="btn btn-primary" style="color:#fff;">Confirmar</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.select2').select2({ theme: 'bootstrap-5'});
+    });
+</script>
+@endsection
+
+@section('footerScript')
+    <script src="{{ URL::asset('/js/pages/mascaras.init.js') }}"></script>
+@endsection
