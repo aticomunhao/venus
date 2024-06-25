005d4bc60411470de3c0420126870bec4bb58809
David <gabrielgalvao10@gmail.com>
2024-06-25 18:30:11 -0300

    retirada de try catch

--- app/Http/Controllers/SalaController.php
+++ app/Http/Controllers/SalaController.php
@@ -143,7 +143,8 @@ class SalaController extends Controller
 
     public function store(Request $request)
     {
-        try{
+       
+        // try{
 
         $ar_condicionado = isset($request->ar_condicionado) ? 1 : 0;
 
@@ -200,12 +201,12 @@ class SalaController extends Controller
 
 
         return redirect('gerenciar-salas');
-    }
-    catch(\Exception $e){
+    // }
+    // catch(\Exception $e){
 
-        $code = $e->getCode( );
-        return view('administrativo-erro.erro-inesperado', compact('code'));
-            }
+    //     $code = $e->getCode( );
+        // return view('administrativo-erro.erro-inesperado', compact('code'));
+        //     }
         }
 
     public function edit($id)
