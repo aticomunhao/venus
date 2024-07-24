b8533ca68ab7aa87d1cfcca14e04763cd71a0a3c
nathanFAO <nathan1fellipe@gmail.com>
2024-07-24 10:05:36 -0300

    v0.1.7/3 - Parcial, Teste Produção

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
     <title>@yield('title')</title>
 
     <!-- Fonts -->
-
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <!-- Link ke jQuery dari CDN -->
