760af4e6bbd30c94df694a74384afb47153f4db2
Moises <moisesshalon72@gmail.com>
2024-08-01 16:56:28 -0300

    teste reu

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,12 @@ import './bootstrap';
 
 import '../sass/app.scss';
 
-import "/node_modules/select2/dist/css/select2.css";
 import 'select2';
+
 import 'select2/dist/css/select2.min.css';
-import 'bootstrap5-toggle/js/bootstrap5-toggle.jquery'
+
+import 'bootstrap5-toggle/js/bootstrap5-toggle.jquery';
+
 import $ from 'jquery';
 
 
