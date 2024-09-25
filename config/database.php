df849f22f872609f019631f6fe024e0909f4d8e0
Moises <moisesshalon72@gmail.com>
2024-09-25 11:22:06 -0300

    Apontamento do database.php

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '192.168.1.137'),
+            'host' => env('DB_HOST', '192.168.1.242'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'pessoal'),
             'username' => env('DB_USERNAME', 'postgres'),
