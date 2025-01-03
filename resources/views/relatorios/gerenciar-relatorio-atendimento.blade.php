5362a4ee74af46b7b7ef0c1308630b804aa6a601
Daviddgabriel <127364906+Daviddgabriel@users.noreply.github.com>
2025-01-03 20:43:30 -0300

    Relatório Atendimento

--- resources/views/relatorios/gerenciar-relatorio-atendimento.blade.php
+++ resources/views/relatorios/gerenciar-relatorio-atendimento.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
-
-@section('title', 'Relatório de Atendimentos')
-
+@section('title')
+    Relatório de Atendimentos
+@endsection
 @section('content')
-    <div class="container-fluid">
-        <h4 class="card-title" style="font-size: 22px; text-align: left; color: #333; font-family: 'Calibri', sans-serif;">
-            Relatório de Atendimentos
-        </h4>
-        <br>
+    <br />
+
+
+
+    <button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+        <i class="bi bi-arrow-up"></i>
+    </button>
+
+
+
+    <div class="container">
+
         <!-- Filtro por datas e botões -->
         <form method="GET" action="{{ url('/gerenciar-relatorio-atendimento') }}">
             <div class="row align-items-center mb-4">
@@ -21,48 +28,102 @@
                     <label for="dt_fim" class="form-label">Data de Fim</label>
                     <input type="date" class="form-control" id="dt_fim" name="dt_fim" value="{{ $dt_fim }}">
                 </div>
-                {{-- <div class="col-md-3">
-                    <label for="status_atendimento" class="form-label">Status</label>
+                <div class="col-md-3">
+                    <label for="status_atendimento" class="form-label">Tipo Relatorio</label>
                     <select class="form-select" id="status_atendimento" name="status_atendimento">
-                        <option value="6" @if(old('status_atendimento') == 6) selected @endif>Finalizado</option>
-                        <option value="7" @if(old('status_atendimento') == 7) selected @endif>Cancelado</option>
+                        <option value="1" @if (old('status_atendimento') == 1) selected @endif>Status</option>
+                        <option value="2" @if (old('status_atendimento') == 2) selected @endif>Sexo</option>
                     </select>
-                </div> --}}
+                </div>
                 <!-- Botões -->
                 <div class="col-md-2 d-flex align-items-end">
                     <button type="submit" class="btn btn-light w-100 me-2"
                         style="font-size: 0.9rem; box-shadow: 1px 2px 5px #000000;">
                         Pesquisar
                     </button>
-                    <a href="{{ url('/gerenciar-relatorio-atendimento') }}" <button type="button"
-                        class="btn btn-light w-100"
+                    <a href="{{ url('/gerenciar-relatorio-atendimento') }}" class="btn btn-light w-100"
                         style="font-size: 0.9rem; box-shadow: 1px 2px 5px  #000000; margin-top: 27px;">
                         Limpar
                     </a>
                 </div>
             </div>
         </form>
-        <hr>
-        <!-- Tabela de Resumo -->
-        <div class="table-responsive mb-4">
-            <table class="table table-bordered table-hover align-middle">
-                <thead class="text-center" style="background-color: #f0f0f0; color: #333;">
-                    <tr>
-                        <th>ATENDIDOS</th>
-                        <th>CANCELADOS</th>
-                        <th>MENORES ATENDIDOS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="text-center">{{ $finalizados }}</td>
-                        <td class="text-center">{{ $cancelados }}</td>
-                        <td class="text-center">{{ $menores }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <br />
+        <div class="card">
+            <div class="card-header">
+                Relatório de Atendimentos
+            </div>
+            <div class="card-body" id="printTable">
+                <canvas id="myChart"></canvas>
 
+                <table class="table  table-striped table-bordered border-secondary table-hover align-middle mt-5">
+                    <thead style="text-align: center;">
+                        <tr style="background-color: #d6e3ff; font-size:14px; color:#000000">
+                            <th>TIPO</th>
+                            <th>TOTAL</th>
+                        </tr>
+                    </thead>
+                    <tbody style="font-size: 14px; color:#000000; text-align: center;">
+                        @foreach ($dadosChart as $key => $dado)
+                            <tr>
+                                <td> {{ $key }} </td>
+                                <td> {{ $dado }} </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+
+
+    <style>
+        #btn-back-to-top {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            display: none;
+        }
+    </style>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        let a = []
+        let i = 0
+        let atendimentos = @JSON($dadosChart);
+
+        for (const [key, value] of Object.entries(atendimentos)) {
+            a[i] = {
+                label: `${key}`,
+                data: [`${value}`],
+                borderWidth: 2,
+            }
+            i++
+        }
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Atendimentos (' + @JSON(date('d/m/Y', strtotime($dt_inicio))) + ' - ' +
+                    @JSON(date('d/m/Y', strtotime($dt_fim))) +
+                    ')'
+                ],
+                datasets: a
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                responsive: true,
+
+            }
+        });
+    </script>
 @endsection
