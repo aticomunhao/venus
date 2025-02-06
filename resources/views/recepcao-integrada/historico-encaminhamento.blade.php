50432340a7255f9c7677cb78a9b1d2dba93fbf0e
Daviddgabriel <127364906+Daviddgabriel@users.noreply.github.com>
2025-02-06 18:51:29 -0300

    Presença emergência v1

--- resources/views/recepcao-integrada/historico-encaminhamento.blade.php
+++ resources/views/recepcao-integrada/historico-encaminhamento.blade.php
@@ -6,13 +6,9 @@
 
 @section('content')
     <br>
-    <button
-        type="button"
-        class="btn btn-danger btn-floating btn-lg"
-        id="btn-back-to-top"
-        >
-  <i class="bi bi-arrow-up"></i>
-</button>
+    <button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+        <i class="bi bi-arrow-up"></i>
+    </button>
 
     <div class="container">
         <div class="row justify-content-center">
@@ -83,7 +79,6 @@
                 {{-- Fim Card Dados de Atendimento --}}
 
                 <br>
-
                 {{-- Inicio Card do Atendimento Atual --}}
                 <div class="card">
                     <div class="card-header">
@@ -160,63 +155,104 @@
 
                     </div>
                 </div>
+                <br>
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                               TRATAMENTO DE EMERGÊNCIA
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                            data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <table class="table table-sm table-bordered table-striped">
+                                    <thead style="text-align:center; background: #daffe0;">
+                                        <tr style="text-align:center; font-weight: bold; font-size:12px">
+                                            <td class="col">NR</td>
+                                            <td class="col">DATA</td>
+                                            <td class="col">GRUPO</td>
+                                            <td class="col">PRESENÇA</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($emergencia as $lists1)
+                                            <tr style="text-align:center; font-size:13px">
+                                                <td>{{ $lists1->idp }}</td>
+                                                <td>{{ $lists1->data }}</td>
+                                                <td>{{ $lists1->nome }}</td>
+                                                @if ($lists1->presenca == 1)
+                                                    <td style="background-color:#90EE90;">Sim</td>
+                                                @elseif ($lists1->presenca == 0)
+                                                    <td style="background-color:#FA8072;">Não</td>
+                                                @endif
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 {{-- Fim card Tratamento/Encaminhamento --}}
 
                 <br>
 
                 {{-- Inicio Card Outros Encaminhamentos --}}
-                @if(count($encaminhamentosAlternativos) > 0)
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col">
-                                OUTROS ENCAMINHAMENTOS
+                @if (count($encaminhamentosAlternativos) > 0)
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col">
+                                    OUTROS ENCAMINHAMENTOS
+                                </div>
                             </div>
                         </div>
+                        <div class="card-body">
+                            @foreach ($encaminhamentosAlternativos as $encaminhamento)
+                                <table class="table table-sm table-bordered table-striped">
+                                    <thead style="text-align:center; background: #daffe0;">
+                                        <tr style="text-align:center; font-weight: bold; font-size:12px">
+                                            <td class="col">NR</td>
+                                            <td class="col">INICIO</td>
+                                            <td class="col">FINAL</td>
+                                            <td class="col">TRATAMENTO</td>
+                                            <td class="col">GRUPO</td>
+                                            <td class="col">DIA</td>
+                                            <td class="col">HORÁRIO</td>
+                                            <td class="col">STATUS</td>
+                                            <td class="col">LINK</td>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody>
+                                        <tr style="text-align:center;font-size:13px">
+                                            <td>{{ $encaminhamento->idt ?? $encaminhamento->ide }}</td>
+                                            <td>{{ $encaminhamento->dt_inicio != null ? date('d/m/Y', strtotime($encaminhamento->dt_inicio)) : '-' }}
+                                            </td>
+                                            <td>{{ $encaminhamento->dt_fim != null ? date('d/m/Y', strtotime($encaminhamento->dt_fim)) : '-' }}
+                                            </td>
+                                            <td>{{ $encaminhamento->descricao }}</td>
+                                            <td>{{ $encaminhamento->nome }}</td>
+                                            <td>{{ $encaminhamento->dia }}</td>
+                                            <td>{{ $encaminhamento->h_inicio }}</td>
+                                            <td>{{ $encaminhamento->status }}</td>
+                                            <td>
+                                                <a class="btn btn-sm tooltips"
+                                                    href="/visualizar-enc/{{ $encaminhamento->ide }}"
+                                                    style="text-align:right;" role="button">
+                                                    <span class="tooltiptext">Visualizar</span>
+                                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            @endforeach
+                        </div>
                     </div>
-                    <div class="card-body">
-                        @foreach($encaminhamentosAlternativos as $encaminhamento)
-
-                        <table class="table table-sm table-bordered table-striped">
-                            <thead style="text-align:center; background: #daffe0;">
-                                <tr style="text-align:center; font-weight: bold; font-size:12px">
-                                    <td class="col">NR</td>
-                                    <td class="col">INICIO</td>
-                                    <td class="col">FINAL</td>
-                                    <td class="col">TRATAMENTO</td>
-                                    <td class="col">GRUPO</td>
-                                    <td class="col">DIA</td>
-                                    <td class="col">HORÁRIO</td>
-                                    <td class="col">STATUS</td>
-                                    <td class="col">LINK</td>
-                                </tr>
-
-                            </thead>
-                            <tbody>
-                                <tr style="text-align:center;font-size:13px">
-                                    <td>{{ $encaminhamento->idt ?? $encaminhamento->ide }}</td>
-                                    <td>{{ $encaminhamento->dt_inicio != null ? date('d/m/Y', strtotime($encaminhamento->dt_inicio)) : '-' }}
-                                    </td>
-                                    <td>{{ $encaminhamento->dt_fim != null ? date('d/m/Y', strtotime($encaminhamento->dt_fim)) : '-' }}</td>
-                                    <td>{{ $encaminhamento->descricao }}</td>
-                                    <td>{{ $encaminhamento->nome }}</td>
-                                    <td>{{ $encaminhamento->dia }}</td>
-                                    <td>{{ $encaminhamento->h_inicio }}</td>
-                                    <td>{{ $encaminhamento->status }}</td>
-                                    <td>
-                                        <a class="btn btn-sm tooltips" href="/visualizar-enc/{{ $encaminhamento->ide }}" style="text-align:right;"
-                                        role="button">
-                                        <span class="tooltiptext">Visualizar</span>
-                                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                                    </a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                        @endforeach
-                    </div>
-                </div>
                 @endif
                 {{-- Fim Card Outros Encaminhamentos --}}
 
@@ -229,49 +265,48 @@
             </div>
         </div>
     </div>
-    
+
     <br>
     <br>
     <br>
 
 
-<style>
-    #btn-back-to-top {
-      position: fixed;
-      bottom: 20px;
-      right: 20px;
-      display: none;
-    }
+    <style>
+        #btn-back-to-top {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            display: none;
+        }
     </style>
     <script>
-    //Get the button
-    let mybutton = document.getElementById("btn-back-to-top");
-    
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction();
-    };
-    
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    }
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
-    
-    function backToTop() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+        //Get the button
+        let mybutton = document.getElementById("btn-back-to-top");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+        // When the user clicks on the button, scroll to the top of the document
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
     </script>
 @endsection
 
 @section('footerScript')
-
 @endsection
