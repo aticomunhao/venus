7c638f0ddf1bacd029ff5b9554723ba3b1c4554b
Daviddgabriel <127364906+Daviddgabriel@users.noreply.github.com>
2025-02-27 20:36:57 -0300

    Visualizar Tratamentos v1

--- resources/views/recepcao-integrada/visualizarRI.blade.php
+++ resources/views/recepcao-integrada/visualizarRI.blade.php
@@ -61,7 +61,18 @@
                                                             @endforeach
                                                         </datalist>
                                                     </div>
-                                                 
+                                                    <div class="col-12 mt-3">Tratamento
+                                                        <select class="form-select pesquisa" id="4" name="tratamento"
+                                                            type="number">
+
+                                                            <option value=""></option>
+                                                            <option value="1" {{ 1 == request('tratamento') ? 'selected' : '' }}>Passe Tratamento Desobsessivo</option>
+                                                            <option value="2" {{ 2 == request('tratamento') ? 'selected' : '' }}>Passe Tratamento Intensivo</option>
+                                                            <option value="4" {{ 4 == request('tratamento') ? 'selected' : '' }}>Programa de Apoio a Portadores de Mediunidade Ostensiva</option>
+                                                            <option value="6" {{ 6 == request('tratamento') ? 'selected' : '' }}>Tratamento Fluidoterápico Integral</option>
+
+                                                        </select>
+                                                    </div>
                                                     <div class="col-12 mt-3">Status
                                                         <select class="form-select teste1" id="4" name="status"
                                                             type="number">
